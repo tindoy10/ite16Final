e932445412c8666c0193eb2f7ab2a9573f77f538
oshantt <131771585+oshantt@users.noreply.github.com>
2023-05-31 13:20:15 +0800

    added particles

--- add-new.php
+++ add-new.php
@@ -41,7 +41,7 @@ if (isset($_POST["submit"])) {
 </head>
 
 <body>
-
+<div id="particles-js"></div>
    <header>
    <h1>Add new</h1>
    <a href="logout.php" class="ghost">Logout</a>
@@ -91,7 +91,8 @@ if (isset($_POST["submit"])) {
 
    <!-- Bootstrap -->
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
+   <script src="js/particles.js"></script>
+	<script src="js/app.js"></script>
 </body>
 
 </html>
